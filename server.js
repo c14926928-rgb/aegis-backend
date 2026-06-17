043268e8592183d01ed4f1c2f397f1a9dc47da0e
Chris <c14926928@gmail.com>
2026-06-17 15:02:14 -0400

    fix backend

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-require("dotenv").config();
-
 const express = require("express");
 const cors = require("cors");
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
